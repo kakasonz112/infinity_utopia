50762c913525ce342759f80c4bb1f70a97dcaaea
kakasonzxc <kakason.z211@gmail.com>
2026-02-02 02:36:41 +0800

    add gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Space_Grotesk } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import SiteHeader from "../components/SiteHeader/SiteHeader";
 import Footer from "../components/Footer/Footer";
@@ -35,6 +36,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${spaceGrotesk.variable} antialiased`}
       >
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RFKHFYZ4E5"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {"window.dataLayer = window.dataLayer || [];"}
+          {"function gtag(){dataLayer.push(arguments);}"}
+          {"gtag('js', new Date());"}
+          {"gtag('config', 'G-RFKHFYZ4E5');"}
+        </Script>
         <SiteHeader />
 
         <div className="appBase">
